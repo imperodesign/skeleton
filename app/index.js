f9e0228cd8d032e367ccc62fcce9fd1dbc4d7737
yosh <yoshtalwar@gmail.com>
2014-10-19 15:13:36 +0100

    serve static files

--- app/index.js
+++ app/index.js
@@ -6,8 +6,11 @@ var app = express();
 var log = require('winston').loggers.get('app:server');
 
 // views
-app.set('views', __dirname+'/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
+// static files
+app.use('/assets', express.static(__dirname + '/assets'));
 
 // routes
 [

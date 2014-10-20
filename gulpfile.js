70c01042f8c175d196938bf85596e72355fe83cd
yosh <yoshtalwar@gmail.com>
2014-10-21 00:05:23 +0100

    typofix

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,10 @@ var gulp = require('gulp');
 var requireDir = require('require-dir');
 
 // require individual tasks
-requireDir('./gulp/tasks', { recursive: true });
+requireDir('./gulp/tasks', { recurse: true });
 
 // development task
-gulp.task('develop', ['css'], function () {
-  gulp.start('watch');
-});
+gulp.task('develop', ['css', 'watch']);
 
 // build task
 //gulp.task('watch', ['build'], function () {

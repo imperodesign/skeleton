4bc9996b13206f6e8ffd64440b0298f82c57ac76
yosh <yoshtalwar@gmail.com>
2015-01-13 18:22:16 +0000

    use rupture for media queries

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var config = require('../config');
+var rupture = require('rupture');
 
 gulp.task('css', function () {
   return gulp.src(config.index.styl)
-    .pipe(plugins.stylus())
+    .pipe(plugins.stylus({
+      use: [rupture()]
+    }))
     .pipe(plugins.autoprefixer())
     .pipe(plugins.csso())
     .pipe(gulp.dest(config.dir.dist));

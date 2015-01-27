0233d45a21d4f7285c3b8468e56ff159bf56e46e
yosh <yoshtalwar@gmail.com>
2015-01-27 18:59:26 +0000

    correct gulp watch paths

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,6 +3,6 @@ var plugins = require('gulp-load-plugins')();
 var config = require('../config');
 
 gulp.task('watch', function () {
-  gulp.watch(config.dir.styl + '**/*.styl', ['css']);
-  gulp.watch(config.dir.js + '**/*.js', ['js-dev']);
+  gulp.watch(config.dir.styl + '/**/*.styl', ['css']);
+  gulp.watch(config.dir.js + '/**/*.js', ['js-dev']);
 });
